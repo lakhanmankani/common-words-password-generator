cdaf591144d51dbcf8d35ee9e860e5f0caccebd7
Lakhan Mankani <lakhan.mankani@gmail.com>
2018-11-19 09:31:20 +0000

    Added word capitalization

--- common_words_password_generator.py
+++ common_words_password_generator.py
@@ -1,13 +1,8 @@
 import random
 
-def get_words(n):
-    with open('wordlist', 'r') as f:
-        list = f.read().split('\n')
-        return list[n]
-        
         
 class CommonWordsPasswordGenerator:
-    def __init__(self, words=None, min_len=None, splitter=None):
+    def __init__(self, words=None, min_len=None, splitter=None, capitalize=None):
         if words is None:
             self.words = 4
         else:
@@ -22,6 +17,11 @@ class CommonWordsPasswordGenerator:
             self.splitter = '-'
         else:
             self.splitter = splitter
+
+        if capitalize is None:
+            self.capitalize = False
+        else:
+            self.capitalize = capitalize
             
         with open('wordlist', 'r') as f:
             self.word_list = set(f.read().split('\n'))
@@ -31,8 +31,11 @@ class CommonWordsPasswordGenerator:
         for word in self.word_list:
             if len(word) >= self.min_len:
                 reduced_word_list.add(word)
-        return random.sample(reduced_word_list, self.words)
+        password_words = random.sample(reduced_word_list, self.words)
+        if self.capitalize:
+            password_words = [s.capitalize() for s in password_words]
+        return self.splitter.join(password_words)
     
-if __name__ == "__main__":
-    generator = CommonWordsPasswordGenerator()
+if __name__ == '__main__':
+    generator = CommonWordsPasswordGenerator(words=6, capitalize=True)
     print(generator.generate_password())
